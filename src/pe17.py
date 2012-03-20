c65fcadff6201b71527b76df7895e74ee446c0bb
Rohan Ramakrishna <rohannr@gmail.com>
2012-03-19 22:02:54 -0700

    problem 17

--- src/pe17.py
+++ src/pe17.py
@@ -1,21 +1,35 @@
 import math
 
-ones = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-tens = [ "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
+ones = [" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+tens = [" ", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+
+
 
 oneseq = list(enumerate(ones, start=1))
-tenseq = list(enumerate(tens start = 10))
+tenseq = list(enumerate(tens, start=10))
 
 
 def to_words(num): 
-	if num <= 10:
-		return ones[num -1]
-	else if num <=20
-		return tens[num-11]
+	if (num < 10):
+		return ones[num]
+	elif (num < 20):
+		return teens[num-10]
+	elif (num < 1000):
+		unit = num % 10
+		ten = num/10 % 10
+		hundreds = num/100
+		return ones[hundreds] + " " + ("hundred " if hundreds != 0 else "") + (" and " if hundreds != 0 and (ten != 0 or unit != 0) else "") + (tens[ten] if ten >= 2 else "") + ("-" if ten >=2 and unit !=0 else "") + (teens[unit] if ten ==1 else ones[unit])
+	else:
+		return "one thousand"
 
-total = 0;
-for i in range(1, 10001):
-	total += to_words(i)
+
+total = 0
+for i in range(1,1001):
+	length = len(to_words(i).replace(" ", "").replace("-", ""))
+	print to_words(i).replace(" ", "").replace("-", "")
+	total+= length
 
 
-
+print total
