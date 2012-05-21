21a62f662875a594d53decd25eaa91b3580c3bd2
Rohan Ramakrishna <rohannr@gmail.com>
2012-05-20 21:59:56 -0400

    Solution to Peoblem 11 (Grid Search)

--- src/pe11/pe11.py
+++ src/pe11/pe11.py
@@ -8,15 +8,13 @@ for line in fp:
 
 max_prod = 1;
 
-for i in range(0,4):
-	for j in range(0,4):
-		up 	  = i - 3 >= 0 and ilist[i][j]* ilist[i-1][j]* ilist[i-2][j]* ilist[i-3][j] or 1
-		down  = i + 3 < 4 and ilist[i][j]* ilist[i+1][j]* ilist[i+2][j]* ilist[i+3][j] or 1
-		left  = j - 3 >=0  and ilist[i][j]* ilist[i][j-1]* ilist[i][j-2]* ilist[i][j-3] or 1
-		right = j + 3 < 4 and ilist[i][j]* ilist[i][j+1]* ilist[i][j+2]* ilist[i][j+3] or 1
-		diag1 = (i + 3 < 4 and j + 3 < 4) and ilist[i][j] *ilist[i+1][j+1]*ilist[i+2][j+2]*ilist[i+3][j+3] or 1
-		diag2 = (i - 3 >= 0 and j - 3 >=0) and ilist[i][j] *ilist[i-1][j-1]*ilist[i-2][j-2]*ilist[i-3][j-3] or 1
-		max_prod = max(up, down, left, right,diag1, diag2, max_prod)
+for i in range(0,20):
+	for j in range(0,20):
+		down  = i + 3 < 20 and ilist[i][j]* ilist[i+1][j]* ilist[i+2][j]* ilist[i+3][j] or 1
+		right = j + 3 < 20 and ilist[i][j]* ilist[i][j+1]* ilist[i][j+2]* ilist[i][j+3] or 1
+		diag1 = (i + 3 < 20 and j + 3 < 20) and ilist[i][j] *ilist[i+1][j+1]*ilist[i+2][j+2]*ilist[i+3][j+3] or 1
+		diag2 = (i + 3 < 20 and j - 3 >=0) and ilist[i][j] *ilist[i+1][j-1]*ilist[i+2][j-2]*ilist[i+3][j-3] or 1
+		max_prod = max(down,right,diag1, diag2, max_prod)
 		
 print max_prod
 
