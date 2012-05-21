8834493a25aff75572024586c11887b935d12b50
Rohan Ramakrishna <rohannr@gmail.com>
2012-05-20 21:15:01 -0400

    Solution to P14 based on memoization(caching) and recursion

--- src/pe14.py
+++ src/pe14.py
@@ -1,16 +1,25 @@
-# seq = [0 for x in range(0, 1000001)]
-# seq[1] = 1
 
-def seq(num):
-	if num == 1:
-		return 1
+
+seq = {1:1}  #initializing first memoization
+
+def eval_collatz(n):
+	if n in seq:
+		return seq[n]
+	elif n%2 == 0:
+		n1 = n/2
+		return 1 + eval_collatz(n1)
 	else:
-		return 1 + ( seq(num/2) if x%2==0 else seq(3*num + 1))
+		n1 = 3*n + 1
+		return 1 + eval_collatz(n1)
 
-max_seq=1
+max_seq = 1
+max_terms = 1
 
-for x in range(2,1000001):
-	ctr = seq(x)
-	max_seq = max(ctr, max_seq)
+for x in xrange(1,1000001):
+	terms = eval_collatz(x)
+	seq[x] = terms
+	max_seq = x if terms > max_terms else max_seq
+	max_terms = max(terms, max_terms)
 	
+
 print max_seq
